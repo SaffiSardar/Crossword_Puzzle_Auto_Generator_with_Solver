7235e3a4ddfc9c197aa79c2e60744c30e7e900a8
SaffiSardar <saffisardar2012@gmail.com>
2025-06-23 19:49:34 +0500

    completed

--- dictionary/word_loader.py
+++ dictionary/word_loader.py
@@ -1,10 +1,13 @@
 import nltk
 nltk.download('words')
 
+import random
 from nltk.corpus import words
 
 def load_word_dict_by_length(max_len):
     all_words = words.words()
+    random.shuffle(all_words)  # 💥 Randomize word order
+
     word_dict = {}
 
     for w in all_words:
@@ -17,5 +20,8 @@ def load_word_dict_by_length(max_len):
 
     return word_dict
 
-# word_dict = load_word_dict_by_length(max_len=15)
-# print(word_dict[5][:10])  # print sample 5-letter words
+word_dict = load_word_dict_by_length(max_len=15)
+print(word_dict[5][:10])  # Sample test
+# for length in sorted(word_dict.keys()):
+#     print(f"\n📏 {length}-letter words ({len(word_dict[length])} total):")
+#     print(", ".join(word_dict[length]))
